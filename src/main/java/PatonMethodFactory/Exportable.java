95467d0c6b42842a5c3e542ed6e260549c2f5505
Christopher Díaz <nealthmc@gmail.com>
2023-07-12 20:28:54 -0500

    factory method

---
+++ src/main/java/PatonMethodFactory/Exportable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package PatonMethodFactory;
+
+/**
+ *
+ * @author infrative
+ */
+public interface Exportable {
+    public void exportAsFile(String extension);
+}
